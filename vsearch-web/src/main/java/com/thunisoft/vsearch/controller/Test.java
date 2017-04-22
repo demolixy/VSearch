459daa03e89336e46555e7222289cccbba2908e9
lixiangyang <lixiangyang@thunisoft.com>
2017-04-22 12:12:44 +0800

    [*] 提交测试代码

--- vsearch-web/src/main/java/com/thunisoft/vsearch/controller/Test.java
+++ vsearch-web/src/main/java/com/thunisoft/vsearch/controller/Test.java
@@ -3,6 +3,7 @@
  */
 package com.thunisoft.vsearch.controller;
 
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class Test {
 
     @RequestMapping("/{id}")
     public String first(@PathVariable("id") String id) {
+        LogFactory.getLog(getClass()).error("日志测试");
         return "lixy";
     }
     
